a5389d4c24e131b0ad8d57cf305e0f55207f81f5
Aswini2001 <64783778+Aswini2001@users.noreply.github.com>
2020-06-02 20:09:50 +0530

    sizeof and multiplicaion program is saved

---
+++ multiplication project.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float n1,n2,p;
+	
+	cout<<"enter two numbersto multiply"<<"\n";
+	cout<<"n1: ";
+	cin>>n1;
+	cout<<"n2: ";
+	cin>>n2;
+	p=n1*n2;
+	cout<<"product ="<<p;
+	return 0;
+}
---
+++ sizeof projrct.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ int main()
+{
+int a;
+float b;
+char c;
+double d;
+ cout<<"Size of int: "<<sizeof(int)<<" bytes"<<"\n";
+ cout<<"Size of float: "<<sizeof(float)<<" bytes"<<"\n";
+ cout<<"Size of char: "<<sizeof(char)<<" bytes"<<"\n";
+ cout<<"Size of double: "<<sizeof(double)<<" bytes"<<"\n";
+ return 0;
+	}
